e274426a1c4662590b28c649137490daaac02ee1
JoseVi <josgarser>
2023-06-13 16:36:02 +0200

    V3

--- main2.py
+++ main2.py
@@ -2,14 +2,14 @@ import random
 random = random.randrange(0, 10)
 
 num = int(input("Dime un numero del 0 al 10: "))
-print(random)
+
 if num == random:
     print("Has adivinado el numero!!!")
 else:
     while num != random:
         if num < random:
-            num = int(input("No as acertado el numero es menor que el random: "))
+            num = int(input("El numero es menor que el random: "))
         else:
-            num = int(input("No as acertado el numero es mayor que el random: "))
+            num = int(input("El numero es mayor que el random: "))
 
     print("Has adivinado el numero!!!")
